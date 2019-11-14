91f93e8126888267eb37793282e64cfa21567a48
Yesu Carter <ycarter@colgate.edu>
2019-11-14 11:15:16 -0500

    Working on Game create form

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const express    = require('express');
 const mongoose   = require('mongoose');
 const bodyParser = require('body-parser');
 
-// Init App
-const app = express();
-const port = 3000;
-app.listen(port, () => {
-  console.log('Server started on port ' + port);
-});
+// const config     = require('./config/db.js')
+
+// Use Node's default promise instead of Mongoose's promise library
+mongoose.Promise = global.Promise;
 
 // Set Model
 let Game = require('./models/game.js');
 
 // Setup Database
 mongoose.connect('mongodb://localhost/nodeGame', {useNewUrlParser: true});
+// const connectionString = 'mongodb+srv://new-admin:oqB4x7YyYgjGuJMH@cluster0-zb5l0.mongodb.net/test?retryWrites=true&w=majority';
+// mongoose.connect(connectionString, {useNewUrlParser: true});
 let db = mongoose.connection;
+
 // Check for DB connecion
 db.once('open', () => {
   console.log('Connected to MongoDB');
@@ -24,6 +25,14 @@ db.once('open', () => {
 // Check for DB error
 db.on('error', (err) => {
   console.log(err);
+});
+
+
+// Init App
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
 });
 
 // Set View to HTML files in views folder
@@ -34,12 +43,20 @@ app.set('views', 'views');   // as default extension
 // Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 // Set Home Route
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
   Game.find({}, (err, games) => {
     if (err) {
       console.log(err);
     } else {
+      console.log("HERE");
       if (games.length < 1) {
         console.log("Something went wrong, no games found in database.");
       } else {
@@ -47,8 +64,11 @@ app.get('/', (req, res) => {
       }
     }
   });
+  console.log("rendering games as null");
+  res.render('index', {games: null });
 });
 
+// Get Specific Game Route
 app.get("/games/:id", (request, response) => {
   Game.findById(request.params.id, (err, game) => {
     if (err) {
@@ -59,18 +79,43 @@ app.get("/games/:id", (request, response) => {
   });
 });
 
-app.get("/modern-warfare-2", (req, res) => {
-  res.render('modern-warfare-2');
-});
-
-// Set Get Route
+// Set Get All Games Route
 app.get('/games', (req, res) => {
   Game.find({}, (err, games) => {
-    console.log('Rendering Games: ' + games.length)
+    console.log('Rendering Games: ' + games.length);
     if (err) {
       console.log(err);
     } else {
       res.render('games', { games: games});
     }
   });
+});
+
+// Add Route to host Create Game Form
+app.get('/create', (request, response) => {
+  response.render("create");
+});
+
+// Create A Game Route
+app.post('/create', async(request, response) => { // since mongoose functions are asynchronous
+  // const newGame = new Game({"title": response.params.});
+  console.log(request.body);
+  let newGame = new Game({
+     "title": request.body.titleInput,
+     "rating": request.body.ratingInput,
+     "price": request.body.priceInput,
+     "description": request.body.descriptionInput,
+     "publisher": request.body.publisherInput,
+     "developer": request.body.developerInput
+   });
+   newGame.save((err) =>{
+     if (err) {
+       console.log("Error encountered while saving new Game: " + err);
+       return;
+     } else {
+       console.log("here");
+       response.redirect('/');
+     }
+   });
+    // response.redirect("create");
 });
